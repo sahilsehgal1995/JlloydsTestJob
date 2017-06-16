d563c070554f1e29fe2af2ad3dcc721d65fea593
Sahil Sehgal <sahilsehgal1995@gmail.com>
2017-06-17 02:21:37 +0530

    test job 2 completed

--- src/components/RSS.vue
+++ src/components/RSS.vue
@@ -7,6 +7,19 @@
     <br>
     <div class="problem">
       Implementation: 
+      <br>
+      <input type="text" v-model="url" placeholder="Enter a valid RSS field url">
+      <button v-on:click="getRssFields">Submit</button>
+      <br>
+      {{error}}
+        <ul id="fields">
+          <li v-for="field in fields">
+              <div> 
+                  <div v-html="field.description"></div>
+              </div>
+              <br>
+          </li>
+        </ul>
     </div>
   </div>
 </template>
@@ -16,7 +29,34 @@ export default {
   name: 'RSS',
   data () {
     return {
-      
+      url: 'https://news.google.com/news?cf=all&hl=en&pz=1&ned=in&output=rss',
+      fields: [],
+      error: ''
+    }
+  },
+  methods: {
+    getRssFields: function(event){
+      console.log("getRssFields", event, this.$http.get);
+      var data = {
+        dataType: 'json',
+        params:{
+          rss_url: this.url
+        }
+      }
+      this.$http.get('https://api.rss2json.com/v1/api.json', data)
+      .then(function(response){
+          console.log("response", response);
+          this.fields = [];
+          if (response.body.status === 'ok'){
+            this.error = ''
+            this.fields = response.body.items
+            return true
+          }
+          this.error = response.body.message
+      }, function(error){
+          console.log("error", error);
+          this.error = error.body
+      });
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
 
+Vue.use(VueResource)
+Vue.http.options.emulateJSON = true
 Vue.config.productionTip = false
+// Vue.http.options.root = '';
 
 /* eslint-disable no-new */
 new Vue({
